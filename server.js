82b7090330ee5b6b6809072cf2bf1e1dba52b5ef
Munci Tunc <muncitunckalaycioglu@gmail.com>
2022-09-26 23:20:15 +0300

    PORT updated

--- server.js
+++ server.js
@@ -21,6 +21,6 @@ app.get('/prosgett', (req, res) => {
       res.json(data);
     })
 })
-app.listen(3000, ()=> {
-  console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, ()=> {
+  console.log('app is running on port ${process.env.PORT}');
 })
